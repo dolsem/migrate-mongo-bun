4cd07b8653ae89f31ac0c1d2731d2580e612b275
Denis Olsem <dols3m@gmail.com>
2025-03-03 20:28:40 -0500

    fix issues

--- bin/migrate-mongo-bun.js
+++ bin/migrate-mongo-bun.js
@@ -1,17 +1,16 @@
 import path from 'path';
 
 let configFilename = 'migrate-mongo-config.ts';
-let argv = [...process.argv];
-const fileOptIx = argv.findIndex((arg) => ['-f', '--file'].includes(arg));
+const fileOptIx = process.argv.findIndex((arg) => ['-f', '--file'].includes(arg));
 if (fileOptIx >= 0) {
-  [, configFilename] = argv.splice(fileOptIx, 2);
+  [, configFilename] = process.argv.splice(fileOptIx, 2);
 }
 
 const { default: config } = await import(path.join(process.cwd(), configFilename));
 Object.assign(global, { config });
 
 Bun.write(new URL(import.meta.resolve('migrate-mongo/samples/esm/migration.js')), `
-import { migration } from 'migrate-mongo-ts';
+import { migration } from 'migrate-mongo-bun';
 
 export const up = migration(async (db, session) => {
   // migration here
@@ -20,6 +19,6 @@ export const up = migration(async (db, session) => {
 export const down = migration(async (db, session) => {
   // rollback here
 });
-  `);
+`.trimStart());
   
-await import('migrate-mongo/bin/migrate-mongo' as any);
+await import('migrate-mongo/bin/migrate-mongo');
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import type { Db, MongoClient, ClientSession } from 'mongodb';
+import type { config as c } from 'migrate-mongo';
+
+export type Config = c.Config;
 
 export const migration = (run: (db: Db, session: ClientSession) => Promise<void>) => async (db: Db, client: MongoClient) => {
     const session = client.startSession();
